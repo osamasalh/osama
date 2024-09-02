868d8fe929d7040e9b0da881c7992b258e105ae1
osamasalh <r.10.1999@hotmail.co.uk>
2024-09-03 01:29:06 +0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://stack-overflow-eight.vercel.app/",
+  baseURL: "https://localhost:5000/",
 });
 
 API.interceptors.request.use((req) => {
